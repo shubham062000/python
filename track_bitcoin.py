2c5d89f7be744b9d98e1dd8a9b185b9efff1e3cd
Shubham Torkade <92077877+shubham062000@users.noreply.github.com>
2022-04-10 13:32:35 +0530

    Add files via upload

---
+++ track_bitcoin.py
@@ -0,0 +1,35 @@
+import requests
+import tkinter as tk
+from datetime import datetime
+
+canvas = tk.Tk()
+canvas.geometry('400x500')
+canvas.title('BITCOIN TRACKER')
+
+f1 = ('poppins', 24, 'bold')
+f2 = ('poppins', 22, 'bold')
+f3 = ('poppins', 18, 'bold')
+
+label = tk.Label(canvas, text = 'Bitcoin Price', font= f1)
+label.pack(pady=20)
+
+labelprice = tk.Label(canvas, font= f2)
+labelprice.pack(pady=20)
+
+labeltime = tk.Label(canvas, font= f3)
+labeltime.pack(pady=20)
+
+def trackbitcoin():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    response = requests.get(url).json()
+    price = response['bpi']['USD']['rate']
+    print('Price is :', price , 'US Dollars')
+    time = datetime.now().strftime("%H:%M:%S")
+   
+    labelprice.config(text= str(price) + " $")
+    labeltime.config(text= 'Updated at :' + time)
+
+    canvas.after(1000, trackbitcoin)
+
+trackbitcoin()
+canvas.mainloop()
